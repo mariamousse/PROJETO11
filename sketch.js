6dd8c212a7a5326899da769ded2e49c3d7113eb6
mariamousse <94935704+mariamousse@users.noreply.github.com>
2021-12-08 21:55:55 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 var runner, runner_running;
 var pistaImage, pista
+var sprite1, sprite2;
 function preload(){
   //imagens pré-carregadas
   runner_running = loadAnimation("Runner-1.png", "Runner-2.png");
@@ -13,7 +14,12 @@ function setup(){
   pista.addImage("pathImage", pistaImage)
   runner = createSprite(80,500,400,200);
   runner.addAnimation("runner", runner_running);
+  sprite1 = createSprite(390,300,10,600);
+  sprite2 = createSprite(10,300,10,600);
   
+  sprite1.visible = false;
+  sprite2.visible = false
+   
   pista.scale = 1.2;
   runner.scale = 0.05;
 
@@ -26,4 +32,16 @@ function draw() {
   if(pista.y > 400){
     pista.y = pista.height/4
   }
+  if(runner.x < 350){
+    runner.x = 350;
+  }
+  if(runner.x > 50){
+    runner.x = 50;
+  }
+    runner.x = World.mouseX;
+  
+
+  runner.collide(sprite1);
+  runner.collide(sprite2);
+
 }
